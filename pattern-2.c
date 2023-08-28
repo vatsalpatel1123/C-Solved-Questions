4f6b6e858d0df4944650db6b5605885aa8bd266f
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-28 22:31:34 +0530

    Hallow Diamond Pattern

---
+++ pattern-2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int i,j,rows;
+printf("Enter the number of rows to print the Hallow Diamond: \n");
+scanf("%d",&rows);
+
+
+
+for(i=1; i<=rows; i++){
+for(j=rows; j>i; j--){
+printf(" ");
+}
+printf("*");
+for(j=1; j<(i-1)*2; j++){
+printf(" ");
+}
+if(i==1){
+printf("\n");
+}
+else{
+printf("*\n");
+}
+}
+
+
+for(i=rows-1; i>=1; i--){
+for(j=rows; j>i; j--){
+printf(" ");
+}
+printf("*");
+for(j=1; j<(i-1)*2; j++){
+printf(" ");
+}
+if(i==1){
+printf("\n");
+}
+else{
+printf("*\n");
+}
+}
+
+return 0;
+}

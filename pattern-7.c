cca233f150e54052ab8e1a49d0b3c068314a0f85
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-08-29 19:00:13 +0530

    Inverted Full Pyramid

---
+++ pattern-7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i,j,n;
+    printf(" -  Inverted Full Pyramid - \n");
+    printf("Enter Number : ");
+    scanf("%d",&n);
+   
+
+    for(i = 1; i<=n; i++){
+
+        for(j=1; j<=n; j++){
+            if(i<=j){
+            printf("* ");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

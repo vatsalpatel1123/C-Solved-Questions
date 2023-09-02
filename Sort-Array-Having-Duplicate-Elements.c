44563cddb32b22f24494da1b0d7d0105e4d4e656
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-02 23:54:50 +0530

    Sort Array having duplicate element

---
+++ Sort-Array-Having-Duplicate-Elements.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+void swap(int *a, int *b) 
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+int partition(int array[], int low, int high) 
+{  
+  int pivot = array[high];
+  int i = (low - 1);
+  for (int j = low; j < high; j++) 
+  {
+    if (array[j] <= pivot) 
+    {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+void quickSort(int array[], int low, int high) 
+{
+  if (low < high) 
+  {    
+    
+    int pi = partition(array, low, high);    
+    quickSort(array, low, pi - 1);    
+    quickSort(array, pi + 1, high);
+  }
+}
+void printArray(int array[], int size) 
+{
+  for (int i = 0; i < size; ++i) 
+  {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+int main() 
+{
+  int arr[] = {3 , 5 ,7 ,3 ,4,2 ,2, 8};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  quickSort(arr, 0, n - 1);
+  printf("Sorted array in ascending order: \n");
+  printArray(arr, n);
+}
